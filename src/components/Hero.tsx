c00305d82a453a74fce58c783eeb90261cbdc6cc
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 00:45:31 +0100

    section work

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-81px)] relative px-6 py-8 lg:px-28">
+      Hero
+    </section>
+  );
+};
+
+export default Hero;
