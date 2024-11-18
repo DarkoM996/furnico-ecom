1b0716fbb775c2cc7a08a58f024d858810031850
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 00:09:19 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <h1 className="text-7xl py-4 ">Welcome to this React typescript App</h1>
+      <div>
+        <Button>Shadcn</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
