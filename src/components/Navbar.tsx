0a0aa5347b6701fe21ced25f0356ece59698e688
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 00:13:55 +0100

    initial design set up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,23 @@
+import { MenuIcon, SearchIcon, ShoppingBag } from "lucide-react";
+
 const Navbar = () => {
   return (
     <nav className="border-b border-stone-200">
       <div className="max-w-screen-2xl mx-auto w-full h-20 flex justify-between items-center px-6">
         <div>
           <h4 className="text-2xl md:text-3xl">Furnico</h4>
+        </div>
+        <ul className="flex gap-2">
+          <a href="/">Home</a>
+          <a href="/">Products</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+        </ul>
+        {/* Icons */}
+        <div className="flex gap-2">
+          <MenuIcon />
+          <ShoppingBag />
+          <SearchIcon />
         </div>
       </div>
     </nav>
