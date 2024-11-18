7ce514a156fdb394b4d21aefc1d6097495f5035a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 00:15:58 +0100

    classes work now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { MenuIcon, SearchIcon, ShoppingBag } from "lucide-react";
 const Navbar = () => {
   return (
     <nav className="border-b border-stone-200">
-      <div className="max-w-screen-2xl mx-auto w-full h-20 flex justify-between items-center px-6">
+      <div className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center px-6 lg:px-28">
         <div>
           <h4 className="text-2xl md:text-3xl">Furnico</h4>
         </div>
