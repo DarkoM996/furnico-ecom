713139b9246bf1e62c82209d4a3dee3c3cdadbc6
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 17:12:17 +0100

    setting up section

--- src/components/LatestProducts.tsx
+++ src/components/LatestProducts.tsx
@@ -1,9 +1,56 @@
 const LatestProducts = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-6 lg:px-28">
-      <h2>Latest Products</h2>
+      <h2 className="pb-12 text-3xl md:text-4xl lg:text-5xl">
+        Latest Products
+      </h2>
       {/* Grid */}
-      <div></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {/* Product 1 */}
+        <div>
+          {/* Image */}
+          <div>
+            <p>Image</p>
+          </div>
+          {/* Text */}
+          <div>
+            <p>Text</p>
+          </div>
+        </div>
+        {/* Product 1 */}
+        <div>
+          {/* Image */}
+          <div>
+            <p>Image</p>
+          </div>
+          {/* Text */}
+          <div>
+            <p>Text</p>
+          </div>
+        </div>
+        {/* Product 1 */}
+        <div>
+          {/* Image */}
+          <div>
+            <p>Image</p>
+          </div>
+          {/* Text */}
+          <div>
+            <p>Text</p>
+          </div>
+        </div>
+        {/* Product 1 */}
+        <div>
+          {/* Image */}
+          <div>
+            <p>Image</p>
+          </div>
+          {/* Text */}
+          <div>
+            <p>Text</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
