94c446a7eafeb90f0259eeb0e6aa6b24ad827a30
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 19:01:18 +0100

    latest

--- src/components/FrequentlyAskedQuestions.tsx
+++ src/components/FrequentlyAskedQuestions.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-
 const FrequentlyAskedQuestions = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-6 lg:px-28 lg:py-24">
-      <h2 className="text-3xl md:text-4xl lg:text-5xl pb-12">
-        Frequently Asked Questions
-      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full h-fit">
+        <div>
+          <h2 className="text-3xl md:text-4xl lg:text-5xl pb-12">
+            Frequently Asked Questions
+          </h2>
+        </div>
+        {/* Shadcn stuff */}
+        <div></div>
+      </div>
     </section>
   );
 };
