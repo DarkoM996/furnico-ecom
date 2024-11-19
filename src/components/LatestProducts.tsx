670eb7cfa32ef39fee5646478bd0958d29175825
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 17:17:58 +0100

    latest

--- src/components/LatestProducts.tsx
+++ src/components/LatestProducts.tsx
@@ -1,3 +1,6 @@
+import { Heart } from "lucide-react";
+import image from "../images/trendingproduct.jpg";
+
 const LatestProducts = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-6 lg:px-28">
@@ -9,12 +12,20 @@ const LatestProducts = () => {
         {/* Product 1 */}
         <div>
           {/* Image */}
-          <div>
-            <p>Image</p>
+          <div className="relative w-full h-full">
+            <img
+              src={image}
+              alt="product image"
+              className="w-full h-full object-cover rounded-3xl"
+            />
           </div>
           {/* Text */}
-          <div>
-            <p>Text</p>
+          <div className="flex flex-col justify-center items-start gap-4 w-full h-fit">
+            <div className="flex flex-row justify-between items-center w-full">
+              <h4 className="text-2xl md:text-4xl">Product Name</h4>
+              <Heart size={28} />
+            </div>
+            <p>$1000</p>
           </div>
         </div>
         {/* Product 1 */}
