c3591b49507ae514e5f0240eaf353aff2f23b845
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 19:04:56 +0100

    importing shadcn accordion

--- src/components/FrequentlyAskedQuestions.tsx
+++ src/components/FrequentlyAskedQuestions.tsx
@@ -1,14 +1,34 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 const FrequentlyAskedQuestions = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-6 lg:px-28 lg:py-24">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full h-fit">
-        <div>
-          <h2 className="text-3xl md:text-4xl lg:text-5xl pb-12">
-            Frequently Asked Questions
-          </h2>
-        </div>
+        <div>asdasd</div>
         {/* Shadcn stuff */}
-        <div></div>
+        <div>
+          <div>
+            <h2 className="text-3xl md:text-4xl lg:text-5xl pb-12">
+              Frequently Asked Questions
+            </h2>
+          </div>
+          {/* Shadcn accordion #1 */}
+          <div>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+        </div>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,38 +3,60 @@ export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      colors: {
-        primary_lighterShade: {
-          50: "#FCF3E3",
-          100: "#F2E9DA",
-          150: "#E5DDCE",
-          200: "#D9D1C4",
-          300: "#CCC5B8",
-          400: "#BFB9AD",
-          500: "#B2ACA1",
-        },
-        primary_darkerShade: {
-          600: "#595348",
-          700: "#4A453C",
-          750: "#3D3932",
-          800: "#2F2C26",
-          850: "#1F1D19",
-          900: "#12110F",
-        },
-      },
-      fontFamily: {
-        serif: ["Cormorant Garamound", "serif"],
-      },
-      screens: {
-        "3xl": "1920px",
-      },
-    },
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			primary_lighterShade: {
+  				'50': '#FCF3E3',
+  				'100': '#F2E9DA',
+  				'150': '#E5DDCE',
+  				'200': '#D9D1C4',
+  				'300': '#CCC5B8',
+  				'400': '#BFB9AD',
+  				'500': '#B2ACA1'
+  			},
+  			primary_darkerShade: {
+  				'600': '#595348',
+  				'700': '#4A453C',
+  				'750': '#3D3932',
+  				'800': '#2F2C26',
+  				'850': '#1F1D19',
+  				'900': '#12110F'
+  			}
+  		},
+  		fontFamily: {
+  			serif: ["Cormorant Garamound", "serif"]
+  		},
+  		screens: {
+  			'3xl': '1920px'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 };
