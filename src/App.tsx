c01f5222b48c954091a5a9ce907343d6a0a74ffb
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 17:09:07 +0100

    new section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import LatestProducts from "./components/LatestProducts";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <>
       <Navbar />
       <Hero />
+      <LatestProducts />
     </>
   );
 };
---
+++ src/components/LatestProducts.tsx
@@ -0,0 +1,11 @@
+const LatestProducts = () => {
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-6 lg:px-28">
+      <h2>Latest Products</h2>
+      {/* Grid */}
+      <div></div>
+    </section>
+  );
+};
+
+export default LatestProducts;
