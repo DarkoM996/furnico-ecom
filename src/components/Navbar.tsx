0541ffd86f704c23fd3bd260c12a76ed8f08500f
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 16:49:07 +0100

    design adjustment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { MenuIcon, SearchIcon, ShoppingBag } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="border-b border-stone-200">
+    <nav className="border-b border-primary_lighterShade-100">
       <div className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center px-6 lg:px-28">
         <div>
           <h4 className="text-2xl md:text-3xl">Furnico</h4>
