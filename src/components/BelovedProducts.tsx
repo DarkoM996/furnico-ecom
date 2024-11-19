9e6ccd1ad7ff73f9f7a48b8b33b651d480eea90c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-19 18:20:14 +0100

    latest

--- src/components/BelovedProducts.tsx
+++ src/components/BelovedProducts.tsx
@@ -1,7 +1,10 @@
 const BelovedProducts = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-6 lg:py-24 lg:px-28">
-      BelovedProducts
+      <h2 className="text-3xl md:text-4xl lg:text-5xl lg:leading-tight pb-12">
+        Beloved Products By Our Customers
+      </h2>
+      {/* Grid */}
     </section>
   );
 };
