dae4f3feeb7fe23c502fa0cb98033dea095e194c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-20 21:01:05 +0100

    bringing in other parts for the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
+import BelovedProducts from "@/components/BelovedProducts";
 import Hero from "@/components/Hero";
+import LatestProducts from "@/components/LatestProducts";
+import Testimonials from "@/components/Testimonials";
 import React from "react";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <LatestProducts />
+      <BelovedProducts />
+      <Testimonials />
     </>
   );
 };
