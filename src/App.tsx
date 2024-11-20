16e5f5c8cf8b9498278aed0b180b9b47f9fe36b3
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-20 21:03:22 +0100

    route requires path to work correctly

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import AboutPage from "./pages/AboutPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
     </Route>
   )
 );
