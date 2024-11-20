0057016047b044facba3cb88bd695039295aec3e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-20 15:51:00 +0100

    footer done for now

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,42 +2,39 @@ import { Facebook, Instagram, Youtube } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-8 lg:px-28 lg:py-24 bg-primary_darkerShade-900 text-primary_lighterShade-50">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-12 lg:gap-32">
-        <div>
-          <h4>Furnico</h4>
-          <p>
+    <footer className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-8 lg:px-28 lg:py-24 bg-primary_darkerShade-900 text-primary_lighterShade-200">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12 lg:gap-32 lg:py-8">
+        <div className="grid grid-cols-1 place-content-start gap-4">
+          <h4 className="text-2xl md:text-3xl">Furnico</h4>
+          <p className="leading-normal">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
             non dolore praesentium adipisci, ipsum, beatae officia natus error
             expedita numquam provident, molestias odio sequi enim voluptate
             obcaecati saepe atque eaque!
           </p>
         </div>
-        <div>
-          <h4>Other Value</h4>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
+        <div className="grid grid-cols-1 place-content-start gap-4">
+          <h4 className="text-xl md:text-2xl">Quick Links</h4>
+          <div className="grid grid-cols-1 place-content-start gap-2">
+            <a href="/">Products</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <a href="/">Home</a>
+          </div>
         </div>
-        <div>
-          <h4>Other Value</h4>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
-        </div>
-        <div>
-          <h4>Other Value</h4>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
-          <a href="/">Link 1</a>
+        <div className="grid grid-cols-1 place-content-start gap-4">
+          <h4 className="text-xl md:text-2xl">Favorite Products</h4>
+          <div className="grid grid-cols-1 place-content-start gap-2">
+            <a href="/">Product #1</a>
+            <a href="/">Product #2</a>
+            <a href="/">Product #3</a>
+            <a href="/">Product #4</a>
+          </div>
         </div>
       </div>
       <div className="flex flex-row justify-between items-center gap-8">
         <div className="py-8">
-          <p>Some title here</p>
+          <p>© Furnico</p>
         </div>
         <div className="flex flex-row justify-center items-center gap-4">
           <Instagram />
