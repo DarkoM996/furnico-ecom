b1650a6739e57c2615d5ed516a6a50a0dc8d338a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-21 18:35:29 +0100

    new section

---
+++ src/components/aboutpagecomponents/ValueSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ValueSection = () => {
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-6 md:py-10 md:px-16 lg:px-28">
+      ValueSection
+    </section>
+  );
+};
+
+export default ValueSection;
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,9 +1,11 @@
 import AboutPageHero from "@/components/aboutpagecomponents/AboutPageHero";
+import ValueSection from "@/components/aboutpagecomponents/ValueSection";
 
 const AboutPage = () => {
   return (
     <>
       <AboutPageHero />
+      <ValueSection />
     </>
   );
 };
