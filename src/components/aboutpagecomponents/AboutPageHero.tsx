616319d94c51420bc3b8b63ba70be07ae679d2a6
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-21 18:22:11 +0100

    latest

--- src/components/aboutpagecomponents/AboutPageHero.tsx
+++ src/components/aboutpagecomponents/AboutPageHero.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import aboutimage from "../../images/aboutimage.jpg";
 
 const AboutPageHero = () => {
-  return <div>AboutPageHero</div>;
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-81px)] relative px-6 py-8 md:px-16 lg:px-28">
+      <div className="relative w-full h-full">
+        <img
+          src={aboutimage}
+          alt="about image"
+          className="w-full h-full object-cover rounded-3xl"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default AboutPageHero;
