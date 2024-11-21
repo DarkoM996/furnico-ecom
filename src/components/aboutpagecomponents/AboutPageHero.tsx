2aa399c63a7ffc80f7545185113c3c418bda227f
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-21 18:29:59 +0100

    section done for now

--- src/components/aboutpagecomponents/AboutPageHero.tsx
+++ src/components/aboutpagecomponents/AboutPageHero.tsx
@@ -1,3 +1,4 @@
+import { ArrowDown } from "lucide-react";
 import aboutimage from "../../images/aboutimage.jpg";
 
 const AboutPageHero = () => {
@@ -9,6 +10,18 @@ const AboutPageHero = () => {
           alt="about image"
           className="w-full h-full object-cover rounded-3xl"
         />
+        {/* Container with text */}
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-black/50 rounded-3xl text-white">
+          <div className="flex flex-col justify-center items-center gap-6 md:gap-8 lg:gap-14">
+            <h1 className="text-4xl md:text-5xl lg:text-7xl text-center leading-tight lg:leading-tight lg:w-2/3">
+              Feel The Comfort Of Resting Comfortably Using FurniPro
+            </h1>
+            <div className="flex flex-col justify-center items-center gap-2">
+              <button>Swipe Down</button>
+              <ArrowDown />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
