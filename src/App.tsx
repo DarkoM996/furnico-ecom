407be9c99755c16fb527e09056443a94278e4850
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-21 18:07:28 +0100

    pages brought in for routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import AboutPage from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
+import Contact from "./pages/Contact";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/contact" element={<Contact />} />
     </Route>
   )
 );
