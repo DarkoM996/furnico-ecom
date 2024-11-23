eca21d75ef86897188be23e3d2d14b1ba42f8f8c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-23 21:17:24 +0100

    latest

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import contactimage from "../images/contactimage.jpg";
 
 const Contact = () => {
   return (
-    <section className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-8 md:px-10 py:py-12 lg:px-28 lg:py-24">
-      Contact
+    <section className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-8 md:px-10 py:py-12 lg:px-28">
+      <div className="relative h-[300px] lg:h-[400px]">
+        <img
+          src={contactimage}
+          alt="contact image"
+          className="w-full h-full object-cover rounded-3xl"
+        />
+      </div>
     </section>
   );
 };
