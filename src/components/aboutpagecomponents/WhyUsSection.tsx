1926d63544bf95384f8c90abc56ed14dec226419
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-23 20:16:33 +0100

    section work

--- src/components/aboutpagecomponents/WhyUsSection.tsx
+++ src/components/aboutpagecomponents/WhyUsSection.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const WhyUsSection = () => {
-  return <div>WhyUsSection</div>;
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8 md:px-16 md:py-12 lg:px-28 lg:py-24">
+      WhyUsSection
+    </section>
+  );
 };
 
 export default WhyUsSection;
