60f8c1e0ad9f97c6bbaf0e2e020ab470ff2af3a4
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-23 21:14:43 +0100

    new seciton design

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-8 md:px-10 py:py-12 lg:px-28 lg:py-24">
+      Contact
+    </section>
+  );
 };
 
 export default Contact;
