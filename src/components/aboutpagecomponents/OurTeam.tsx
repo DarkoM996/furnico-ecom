5f1dfc219fdea92a144aba2da5e60b2ffb1c01fa
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-23 20:14:20 +0100

    content

--- src/components/aboutpagecomponents/OurTeam.tsx
+++ src/components/aboutpagecomponents/OurTeam.tsx
@@ -1,4 +1,7 @@
 import teammember1 from "../../images/teammember4.jpg";
+import teammember2 from "../../images/teammember3.jpg";
+import teammember3 from "../../images/teammember2.jpg";
+import teammember4 from "../../images/teammember1.jpg";
 
 const OurTeam = () => {
   return (
@@ -21,19 +24,33 @@ const OurTeam = () => {
             </p>
           </div>
         </div>
-        {/* Grid Item #1 */}
+        {/* Grid Item #2 */}
         <div className="grid grid-cols-1 place-content-start gap-6">
           <div className="relative w-full h-[400px]">
             <img
-              src={teammember1}
-              alt="team member 1"
+              src={teammember2}
+              alt="team member 2"
               className="w-full h-full object-cover rounded-3xl"
             />
           </div>
           <div className="grid grid-cols-1 place-content-start gap-3">
-            <h3 className="text-2xl md:text-3xl">John Stokes</h3>
+            <h3 className="text-2xl md:text-3xl">Jenny Stokes</h3>
+            <p className="text-base text-primary_darkerShade-600">CFO</p>
+          </div>
+        </div>
+        {/* Grid Item #3 */}
+        <div className="grid grid-cols-1 place-content-start gap-6">
+          <div className="relative w-full h-[400px]">
+            <img
+              src={teammember4}
+              alt="team member 4"
+              className="w-full h-full object-cover rounded-3xl"
+            />
+          </div>
+          <div className="grid grid-cols-1 place-content-start gap-3">
+            <h3 className="text-2xl md:text-3xl">Sarah Stones</h3>
             <p className="text-base text-primary_darkerShade-600">
-              CEO and Founder
+              Operations Manager
             </p>
           </div>
         </div>
@@ -41,32 +58,14 @@ const OurTeam = () => {
         <div className="grid grid-cols-1 place-content-start gap-6">
           <div className="relative w-full h-[400px]">
             <img
-              src={teammember1}
-              alt="team member 1"
+              src={teammember3}
+              alt="team member 3"
               className="w-full h-full object-cover rounded-3xl"
             />
           </div>
           <div className="grid grid-cols-1 place-content-start gap-3">
-            <h3 className="text-2xl md:text-3xl">John Stokes</h3>
-            <p className="text-base text-primary_darkerShade-600">
-              CEO and Founder
-            </p>
-          </div>
-        </div>
-        {/* Grid Item #1 */}
-        <div className="grid grid-cols-1 place-content-start gap-6">
-          <div className="relative w-full h-[400px]">
-            <img
-              src={teammember1}
-              alt="team member 1"
-              className="w-full h-full object-cover rounded-3xl"
-            />
-          </div>
-          <div className="grid grid-cols-1 place-content-start gap-3">
-            <h3 className="text-2xl md:text-3xl">John Stokes</h3>
-            <p className="text-base text-primary_darkerShade-600">
-              CEO and Founder
-            </p>
+            <h3 className="text-2xl md:text-3xl">Jeffrey Stokes</h3>
+            <p className="text-base text-primary_darkerShade-600">CMO</p>
           </div>
         </div>
       </div>
