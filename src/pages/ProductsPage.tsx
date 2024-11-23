48a9a59bca7db6fd769a1b6c1840299077c15e80
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-23 21:05:30 +0100

    designing

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import productsimg from "../images/productdetailimage2.jpg";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-8 lg:px-28">
+      <div className="relative w-full h-[300px] lg:h-[400px]">
+        <img
+          src={productsimg}
+          alt="products image"
+          className="w-full h-full object-cover rounded-3xl"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default ProductsPage;
