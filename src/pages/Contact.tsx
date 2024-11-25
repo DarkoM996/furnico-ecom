4da259d5d51be8782deb0d172a93a06309ae7f36
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-25 13:47:56 +0100

    latest

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,5 @@
 import contactimage from "../images/contactimage.jpg";
 
-export type Note = {
-  id: number;
-  title: string;
-  content: string;
-};
-
 const Contact = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-fit relative px-6 py-8 md:px-10 py:py-12 lg:px-28">
