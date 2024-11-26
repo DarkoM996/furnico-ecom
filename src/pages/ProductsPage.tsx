2f7ff0a3b045cf1f82e4abcb2c2e25c35c6fda07
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-26 22:21:14 +0100

    changes to filter

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -20,6 +20,72 @@ const ProductsPage = () => {
           </h1>
         </div>
       </div>
+      {/* Filter */}
+      <div className="hidden md:flex py-12">
+        <div className="flex flex-wrap justify-between items-center gap-6">
+          <select
+            name="type"
+            id=""
+            className="py-2 px-4 rounded-full text-sm font-medium bg-primary_lighterShade-200 hover:bg-primary_lighterShade-300"
+          >
+            <option value="">Type</option>
+            <option value="physical">Type</option>
+            <option value="digital">Type</option>
+          </select>
+          <input
+            type="text"
+            name="min"
+            placeholder="min price"
+            className="text-cs rounded-full px-4 py-2 w-32 ring-1 ring-primary_lighterShade-300"
+          />
+          <input
+            type="text"
+            name="max"
+            placeholder="max price"
+            className="text-cs rounded-full px-4 py-2 w-32 ring-1 ring-primary_lighterShade-300"
+          />
+          <select
+            name="type"
+            id=""
+            className="py-2 px-4 rounded-full text-sm font-medium bg-primary_lighterShade-200 hover:bg-primary_lighterShade-300"
+          >
+            <option>Category</option>
+            <option value="">New Arrival</option>
+            <option value="">Popular</option>
+          </select>
+          <select
+            name="type"
+            id=""
+            className="py-2 px-4 rounded-full text-sm font-medium bg-primary_lighterShade-200 hover:bg-primary_lighterShade-300"
+          >
+            <option value="">Color</option>
+            <option value="physical">Type</option>
+            <option value="digital">Type</option>
+          </select>
+          <select
+            name="type"
+            id=""
+            className="py-2 px-4 rounded-full text-sm font-medium bg-primary_lighterShade-200 hover:bg-primary_lighterShade-300"
+          >
+            <option value="">Size</option>
+            <option value="physical">Size</option>
+          </select>
+          {/* Another sort */}
+          <div>
+            <select
+              name=""
+              id=""
+              className="py-2 px-4 rounded-full text-sm font-medium bg-primary_lighterShade-200 hover:bg-primary_lighterShade-300"
+            >
+              <option value="">Sort By</option>
+              <option value="">Price (low to high)</option>
+              <option value="">Price (low to high)</option>
+              <option value="">Newest</option>
+              <option value="">Oldest</option>
+            </select>
+          </div>
+        </div>
+      </div>
       {/* Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-6 py-8 lg:py-24">
         {products.map((product) => (
