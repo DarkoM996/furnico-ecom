dcea88a4d42ef44beb8bbd276940cadd74b40e41
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-26 15:51:01 +0100

    initial work

--- src/pages/Error404Page.tsx
+++ src/pages/Error404Page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Error404Page = () => {
-  return <div>Error404Page</div>;
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8 lg:px-28">
+      Error404Page
+    </section>
+  );
 };
 
 export default Error404Page;
